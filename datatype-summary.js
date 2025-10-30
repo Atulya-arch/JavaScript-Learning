d59b4db106d9b68d0db5e6abacd4304ed8a2c4ae
Atulya-arch <atulyachaturvedi254@gmail.com>
2025-10-30 09:39:10 +0000

    completed till 2hrs

--- datatype-summary.js
+++ datatype-summary.js
@@ -1,33 +1,58 @@
 //Primitive:
 //7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const { captureOwnerStack } = require("react");
 
-const isLogged = false
-const outsideTemp = null
-let userEmail;
+// const score = 100
+// const scoreValue = 100.3
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const isLogged = false
+// const outsideTemp = null
+// let userEmail;
 
-console.log(id == anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-const bigNumber = 12345678910111213141516n
+// console.log(id == anotherId);
+
+// const bigNumber = 12345678910111213141516n
 
 //Reference (Non primitve):
 //Array, Objects, Functions
 
-const heros = ["Aman", "Rahul", "Mukesh", "Ramesh"];
-let myObj = {
-    name: "Avantika", 
-    age: 21, 
+// const heros = ["Aman", "Rahul", "Mukesh", "Ramesh"];
+// let myObj = {
+//     name: "Avantika", 
+//     age: 21, 
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+// myFunction()
+// console.log(typeof bigNumber);
+// console.log(typeof scoreValue);
+// console.log(typeof myFunction);
+
+///////////////////////////////////////////////////////////////////////
+
+//Stack (Primitive): copy jaata hai isliye original value change nhi hoti, 
+//Heap (Non-Primitive): Reference jaata hai isliye original value mei bhi change ho jaata hai
+
+let myProfilename = "aanasharmadotcom"
+let anothername = myProfilename
+anothername = "Hackoverheels"
+
+console.log(myProfilename);
+console.log(anothername);
+
+let user1 = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+let user2 = user1
+user2.email = "abcd@google.com"
 
-console.log(typeof bigNumber);
-console.log(typeof scoreValue);
-console.log(typeof myFunction);
+console.log(user1.email);
+console.log(user2.email);
