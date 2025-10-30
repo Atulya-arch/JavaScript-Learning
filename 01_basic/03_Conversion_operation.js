f7a45378b8edbe7d43cefaee67344a161e6a676d
Atulya-arch <atulyachaturvedi254@gmail.com>
2025-10-30 08:04:19 +0000

    Learnt the Conversion of datatypes

--- 01_basic/03_Conversion_operation.js
+++ 01_basic/03_Conversion_operation.js
@@ -1,17 +1,62 @@
-let score = "54"
+// let score = "54"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// /////////////////////////////////////
 
-let score1 = null
+// let score1 = null
 
-console.log(typeof score1);
-console.log(typeof (score1));
+// console.log(typeof score1);
+// console.log(typeof (score1));
 
-let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+// //////////////////////////////////////
+
+// let score2 = "Anamika"
+
+// console.log(typeof score2);
+// console.log(typeof (score2));
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// /*
+// "35" : 35
+// "35abc" : NaN
+// true -> 1, false -> 0
+// */
+
+// let isloggedIn = 1
+// let isBoolLoggedIn = Boolean(isloggedIn)
+// console.log(isBoolLoggedIn);
+
+// /*
+// 1 -> true, 0 -> false
+// "" : false
+// "Anamika" : true 
+// */
+
+// let someNum = 12
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//**************************************Operations******************************************
+let value = 12
+let negVal = -value
+console.log(negVal)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**3)
+console.log(2%3)
+
