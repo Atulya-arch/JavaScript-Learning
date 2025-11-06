51e69b93a3fc0ec99e95c9bc33152c7dfb2ccfec
Atulya Chaturvedi <atulyachaturvedi254@gmail.com>
2025-11-06 13:18:05 +0000

    objects - part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+//const tensi = new Object() OR,    //  -->Singleton object
+const tensi = {}                    // -->Non-Singleton Object
+console.log(tensi);
+
+tensi.id = "123abc"
+tensi.name = "Sunny"
+tensi.isLoggedIn = false
+
+const regularUser = {
+    email: "abcuhs544@gmail.com",
+    full_name: {
+        userfullName: {
+            first_name: "Anamika",
+            last_name: "Mathur"
+        }
+    }
+}
+
+console.log(regularUser.full_name.userfullName.first_name);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = { obj1, obj2 }
+console.log(obj3);
+
+const obj4 = Object.assign( {}, obj1, obj2 )
+console.log(obj4);
+
+const obj5 = {...obj1, ...obj2}
+console.log(obj5);
