ff4b9f59ae52e8f09ccb6acfd2ccc298a6d474a5
Atulya Chaturvedi <atulyachaturvedi254@gmail.com>
2025-11-11 13:22:52 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,46 @@
-const a = 10
-let b = 20
-var c = 30
+let a = 300
 
-console.log(a);
-console.log(b);
-console.log(c);
+if(true) {
+    let a = 10
+    const b = 20
+    console.log("INNER:", a);
+    //var c = 30
+}
+    
+//var shouln't be used inside the braces
+// console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one() {
+    const username = "Anamika"
+
+    function two() {
+        const hometown = "Delhi"
+        console.log(username);
+    }
+    //console.log(hometown);
+    two()
+}
+one()
+
+if(true){
+    const username = "Sunny"
+
+    if(username === "Sunny"){
+        const hometown = "Jaipur"
+        console.log(username + hometown);
+    }
+    //console.log(hometown);
+}
+//console.log(username);
+
+function addOne(num) {
+    return num + 1
+}
+console.log(addOne(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(6));
