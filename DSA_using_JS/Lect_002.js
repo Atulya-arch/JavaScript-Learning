c1947dbed8a4963caa11acdbe5e618781f0af3df
Atulya Chaturvedi <atulyachaturvedi254@gmail.com>
2026-06-19 23:16:05 +0530

    Modified the last file

--- DSA_using_JS/Lect_002.js
+++ DSA_using_JS/Lect_002.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")();
 
 // Valid Voter Program: 
-var age = Number(prompt("What is your age? ")); // prompt() humesha string mei input leta hai
+let age = Number(prompt("What is your age? ")); // prompt() humesha string mei input leta hai
 console.log(age);
 
 if(isNaN(age) === true) {
@@ -13,3 +13,66 @@ if(isNaN(age) === true) {
 }
 
 // Shop - Discount:
+/* Amount          Discount
+   0 - 5000        0%
+   5001 - 7000     5%
+   7001 - 9000     10%
+   above 9000      20%
+*/
+let amount = Number(prompt("What is the final amount? "));
+if(amount >= 0 && amount <= 5000) {
+    console.log(amount);
+} else if(amount >= 5001 && amount <= 7000) {
+    console.log(amount - (Math.floor((5 * amount) / 100)));
+} else if(amount >= 7001 && amount <= 9000) {
+    console.log(amount - (Math.floor((10 * amount) / 100)));
+} else if(amount >= 9001) {
+    console.log(amount - (Math.floor((20 * amount) / 100)));
+} else {
+    console.log("Invalid amount");
+}
+
+// -------- OR ------------
+
+let amt = Number(prompt("What is the total amount? "));
+let dis;
+if(amt >= 0 && amt <= 5000) {
+    dis = 0;
+} else if(amt >= 5001 && amt <= 7000) {
+    dis = 5;
+} else if(amt >= 7001 && amt <= 9000) {
+    dis = 10;
+} else if(amt >= 9001) {
+    dis = 20;
+} else {
+    console.log("Invalid amount");
+}
+console.log(amt - (Math.floor((dis * amt) / 100))); // keval yhi line repeated hai and ek value ka change hai
+
+// Electricity - Bill :
+/* Units          Price
+   Upto 100       4.2/unit
+   101 - 200      6/unit
+   201 - 400      8/unit
+   above 400      13/unit
+*/
+
+let units = Number(prompt("What are the total units? "));
+let price = 0;
+if(units >= 401) {
+    price += (units - 400) * 13;
+    units = 400;
+}
+if(units >= 201 && units <= 400) {
+    price += (units - 200) * 8;
+    units = 200; 
+}
+if(units >= 101 && units <= 200) {
+    price += (units - 100) * 6;
+    units = 100;
+}
+price += units * 4.2;
+console.log(price);
+
+// INR Denominations:
+let 
