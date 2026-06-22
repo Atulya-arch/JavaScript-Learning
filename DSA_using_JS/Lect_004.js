1888055adbd3fbb251c71ed72f2a78131cd15f90
Atulya Chaturvedi <atulyachaturvedi254@gmail.com>
2026-06-22 22:29:38 +0530

    Added a new file

---
+++ DSA_using_JS/Lect_004.js
@@ -0,0 +1,8 @@
+// PATTERNS:
+// process.stdout.write("Hello ");
+// process.stdout.write("Hello ");
+// process.stdout.write("Hello ");
+
+for(let i=0; i<5; i++) {
+    process.stdout.write("# ");
+}
