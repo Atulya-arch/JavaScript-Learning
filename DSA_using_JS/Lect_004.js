bee608aa8fc72294c7839fce5ac5ba7daa0453ed
Atulya Chaturvedi <atulyachaturvedi254@gmail.com>
2026-06-23 14:23:14 +0530

    Completed Lecture 4

--- DSA_using_JS/Lect_004.js
+++ DSA_using_JS/Lect_004.js
@@ -3,6 +3,88 @@
 // process.stdout.write("Hello ");
 // process.stdout.write("Hello ");
 
-for(let i=0; i<5; i++) {
-    process.stdout.write("# ");
+const prompt = require ("prompt-sync")();
+let n = Number(prompt("Enter a number: "));
+
+for(let i=0; i<n; i++) {
+    for(let j=0; j<n; j++) {
+        process.stdout.write("# ");
+    }
+    console.log();
 }
+
+console.log();
+
+for(let i=0; i<n; i++) {
+    for(let j=0; j<=i; j++) {
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i=1; i<=n; i++) {
+    for(let j=1; j<=i; j++) {
+        process.stdout.write(j + " ");
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i=1; i<=n; i++) {
+    let ascii = 65; // A -> 65
+    for(let j=1; j<=i; j++) {
+        process.stdout.write(String.fromCharCode(ascii) + " "); // NOTE THIS FUNCTION number to character
+        ascii++;
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i=n; i>=1; i--) {
+    for(let j=i; j>=1; j--) {
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i=1; i<=n; i++) {
+    for(let j=n-1; j>=i; j--) {
+        process.stdout.write("  ");
+    }
+    for(let j=1; j<=i; j++) {
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i=1; i<=n; i++) {
+    for(let j=1; j<=n; j++) {
+        if(i + j === n + 1 || i === j) // Only the diagonals
+            process.stdout.write("* ");
+        else
+            process.stdout.write("  ");
+    }
+    console.log();
+}
+
+console.log();
+
+for(let i = 1; i <= n; i++) {
+    for(let j = 1; j < 2*n; j++) {
+        if(i + j === 2 * n || i === j) 
+            process.stdout.write("* "); // We create 2*n - 1 columns for each row
+        else
+            process.stdout.write("  ");
+    }
+    console.log();
+}
+
+console.log();
